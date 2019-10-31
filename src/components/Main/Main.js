d49b7f0ca16af52e313d01f31973e81b2ae2b5a7
Devbuilt <leo@devbuiltlabs.com>
2019-10-31 00:52:42 -0700

    moved banner

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Tuning from '../Tuning/Tuning';
-// import HeaderMain from '../Header/HeaderMain'
+import HeaderMain from '../Header/HeaderMain'
 
 
 class Main extends Component {
@@ -11,8 +11,9 @@ class Main extends Component {
     render() { 
         return ( 
             <div className="AppDarkMode">
-            {/* <HeaderMain/> */}
+            
             <Tuning/>
+            <HeaderMain/>
             </div>
          );
     }
